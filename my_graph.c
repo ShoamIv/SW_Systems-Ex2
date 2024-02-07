f1734373d5660b265f06393ebf884e9015c90dd6
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-02-07 14:18:10 +0200

    Add files via upload

--- my_graph.c
+++ my_graph.c
@@ -5,17 +5,22 @@
 int main(){
 int mat [S][S];
 char user;
-//while user not press D stay in the loop.
+/*
+while user not press D stay in the loop.
+*/
 while(user!='D'){
-//Send to A func. after entering values send to shortest path func to recive the desire matrix.
+/*
+Send to A func. after entering values send to shortest path func to recive the desire matrix.
+*/
   scanf("%c",&user);
     if(user=='A'){
       A(mat);
         shortestpath(mat);
 }
-//send i,j to B and k,t  C.
-//print true or false to B according conditions.
-//print -1 or ans value to C according conditions.
+/*send i,j to B and k,t  C.
+print true or false to B according conditions.
+print -1 or ans value to C according conditions.
+*/
 if (user=='B'){
     int i;
     int j;
