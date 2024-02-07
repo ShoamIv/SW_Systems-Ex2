19211fa03e7325511c3166c258578b30d97c9286
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-02-07 13:10:40 +0200

    Add files via upload

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -13,8 +13,8 @@
         scanf("%d",&weights[i]);
     }
     int prof=knapSack(values,weights,selected_bool);
-    printf("Maximum profit: " "%d",prof);
-    printf("\n""Selected items: b c e");
+    printf("Maximum profit: ""%d",prof);
+    printf("\n""Selected items:");
     for(int i=0;i<arr; i++){
      if(selected_bool[i]==1){
         printf(" %c",items[i]);
