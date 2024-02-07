48bb8527e85086b31e1996c7f522c8a0ab2d1ccd
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-02-07 14:04:15 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -4,6 +4,9 @@
 #define S 10
 #define cap 20
 #define arr 5
+/*
+Fill values in the matrix.
+*/
 void A(int matrix[S][S]){
 
     for(int i=0; i<S; i++){
@@ -12,12 +15,19 @@ void A(int matrix[S][S]){
       }
     }
 } 
+/**
+ * Find minimum and maximum
+*/
 int mini(int a,int b){
   if(a<b){return a;} else return b;
 }
 int maxi(int a,int b){
   if(a>b){return a;} else return b;
 }
+/**
+ * Find the shortest path for every i,j in matrix.
+ * according Floyd Warshall algorithm.
+*/
 void shortestpath(int matrix[S][S]){
 for(int k=0; k<S; k++){
  for(int i=0; i<S; i++){
@@ -39,12 +49,17 @@ for(int k=0; k<S; k++){
 
 return;
 }
+/*
+Send to the shortest path matrix 2 integers.
+*/
 void B(int matrix[S][S],int i,int j){
 int ans=matrix[i][j];
 if(ans==0){printf("False");}else printf("True");
 printf("\n");
 }
-
+/*
+Send to the shortest path matrix 2 integers.
+*/
 void C(int matrix[S][S],int i,int j){
   int ans=matrix[i][j];
   if(ans==0){printf("%d",-1);}
@@ -52,6 +67,9 @@ void C(int matrix[S][S],int i,int j){
   printf("\n");
   return;
 }
+/*
+create and fill knapsack matrix. that we can track back for items.
+*/
 int knapSack (int value[5],int weights[5],int selected_bool[5]){
  int k[arr+1][cap+1];
     for(int i=0; i<=arr; i++){
@@ -66,6 +84,11 @@ int knapSack (int value[5],int weights[5],int selected_bool[5]){
     reverse(k,weights,selected_bool);
    return(k[arr][cap]);
 }
+/*
+Start traverse the matrix from position [5][20](max).
+go "up" in the matrix and check conditions.
+according conditions decrease i and j, and increase selected_bool[i].
+*/
 void reverse(int k[6][21],int weights[5],int selected_bool[5]){
 int i=5, j=cap;
 while(i>0 && j>0){
